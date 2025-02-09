d19343af197178032f9303fc2d0f67fec664af1b
Otonbara Okolai <140208396+Otonbara@users.noreply.github.com>
2025-02-09 06:46:16 +0100

    Update About.jsx
    Code refactor on the Get to Know me <p>

--- otonbara/src/pages/About.jsx
+++ otonbara/src/pages/About.jsx
@@ -64,12 +64,9 @@ const About = () => {
                 <div className='about-content'>
                     <h2>GET TO KNOW ME</h2>
                     <p>
-                        Oh, so you made it this far, that means you are impressed by what you have seen so far.
-                        Well, I am glad you are. I am Otonbara Alfred Okolai a web developer based in Lagos, Nigeria
-                        (I am also a civil engineer if you need professional advice on that, okay!😉), I have suitable Knowledge
+                        I am Otonbara Alfred Okolai a web developer based in Lagos, Nigeria, I have suitable Knowledge
                         on front-end development, and cloud computing. I aspire to be really good at my craft and also clairvoyant to
-                        bring your wildest ideas and imaginations into reality. And I am probably also the chillest guy you will ever meet
-                        or work with. So grab a coffee, popcorn, beer, or whatever you like and let us get to exploring more of this page together......
+                        bring your wildest ideas and imaginations into reality.
                     </p>
                 </div>
                 <div className="resume">
